0fb8b92c124558740bfdae8da51d92321a8c0316
Oeng Mengthong <mengthongoenggl@gmail.com>
2025-01-12 22:50:46 +0700

    remove unuse file

--- src/class/types.ts
+++
@@ -1,8 +0,0 @@
-
-export interface ClassType {
-  id: number;
-  name: string;
-  students: Array<any>; // Replace 'any' with your Student type if available
-  createdAt?: Date;
-  updatedAt?: Date;
-}
